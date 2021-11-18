a9b820b75c5c3b3cc5952b90acc9e1a36b9a1548
Anderson Furlan <alsfurlan@gmail.com>
2021-11-18 19:49:47 -0300

    Cadastro de livros com vínculo de autores

--- src/app/livros/livros-cadastro/livros-cadastro.component.ts
+++ src/app/livros/livros-cadastro/livros-cadastro.component.ts
@@ -1,56 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Autor } from 'src/app/autores/autores.model';
+import { AutoresService } from 'src/app/autores/autores.service';
 import { LivrosService } from '../livros.service';
 
 @Component({
   selector: 'app-livros-cadastro',
   templateUrl: './livros-cadastro.component.html',
-  styleUrls: ['./livros-cadastro.component.css']
+  styleUrls: ['./livros-cadastro.component.css'],
 })
 export class LivrosCadastroComponent implements OnInit {
   livroId: number;
   livrosForm: FormGroup;
+  autores: Autor[];
 
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
     private livrosService: LivrosService,
     private activatedRoute: ActivatedRoute,
+    private autoresService: AutoresService
   ) {
+    this.autores = [];
     this.livroId = 0;
     this.livrosForm = this.formBuilder.group({
       titulo: ['', Validators.required],
-      subtitulo: ['']
+      subtitulo: [''],
+      autores: ['', Validators.required],
     });
   }
 
   ngOnInit(): void {
+    this.autoresService.findAll().subscribe((autores) => this.autores = autores);
+
     const id = this.activatedRoute.snapshot.params.id;
     if (id) {
       this.livroId = parseInt(id);
       this.livrosService
-        .findById(this.livroId)
-        .subscribe((livro) => {
-          this.livrosForm.patchValue({
-            titulo: livro.titulo,
-            subtitulo: livro.subtitulo,
-          });
+      .findById(this.livroId)
+      .subscribe((livro) => {
+        this.livrosForm.patchValue({
+          titulo: livro.titulo,
+          subtitulo: livro.subtitulo,
+          autores: livro.autores,
         });
+      });
     }
   }
 
+  compareAutores(a1: Autor, a2: Autor) {
+    return a1 && a2 ? a1.id === a2.id : false;
+  }
+
   salvar() {
+    console.log(this.livrosForm.value);
     if (this.livroId) {
       this.livrosService
-        .update(this.livroId, this.livrosForm.value)
-        .subscribe(
-          () => {
-            alert('Livro atualizado com sucesso');
-            this.router.navigate(['./livros']);
-          },
-          () => alert('Erro ao atualizar o livro')
-        );
+      .update(this.livroId, this.livrosForm.value)
+      .subscribe(
+        () => {
+          alert('Livro atualizado com sucesso');
+          this.router.navigate(['./livros']);
+        },
+        () => alert('Erro ao atualizar o livro')
+      );
     } else {
       this.livrosService.add(this.livrosForm.value).subscribe(
         () => {
@@ -61,5 +75,4 @@ export class LivrosCadastroComponent implements OnInit {
       );
     }
   }
-
 }
